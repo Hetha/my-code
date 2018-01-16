7ef81e1056ea51b288671e5ffc047810ab24d568
Your Name <you@example.com>
2018-01-16 21:37:39 +0000

    Ostwald

--- Junk.java
+++ Junk.java
@@ -3,9 +3,12 @@ public class Junk{
 public static void main(String[] args){
 System.out.println("inputting " + args[0]);
 
-if (didOstwaldActAlone(Boolean.parseBoolean(args[0])))
+boolean result = didOstwaldActAlone(Boolean.parseBoolean(args[0]));
+
+if (result)
 	{System.out.println("Acted Alone");} 
-else {System.out.println("Did not act alone");}
+else 
+	{System.out.println("Did not act alone");}
 }
 
 private static  boolean didOstwaldActAlone(boolean input){return input;}
